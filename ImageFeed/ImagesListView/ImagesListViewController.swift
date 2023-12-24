1cd43288cd94be773896d57bdbbb3a76513551be
AnnaGurik <minigurk@mail.ru>
2023-12-24 20:00:07 +0500

    add all cells

--- ImageFeed/ImagesListView/ImagesListViewController.swift
+++ ImageFeed/ImagesListView/ImagesListViewController.swift
@@ -1,16 +1,26 @@
 import UIKit
 
 final class ImagesListViewController: UIViewController {
-
     @IBOutlet weak var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -28,6 +38,16 @@ extension ImagesListViewController: UITableViewDataSource {
 
 extension ImagesListViewController {
     func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        if let image = UIImage(named: photosName[indexPath.row]) {
+            cell.cellImage.image = image
+            cell.dateLabel.text = dateFormatter.string(from: Date())
+
+            let isLiked = indexPath.row % 2 == 0
+            let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
+            cell.likeButton.setImage(likeImage, for: .normal)
+        } else {
+            return
+        }
     }
 }
 
