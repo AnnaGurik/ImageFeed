c82d0387ba0df9fb39c5a0f7b305d0667f26010b
AnnaGurik <minigurk@mail.ru>
2023-12-24 19:43:38 +0500

    fix view controller name to images list view controller and add background color

--- ImageFeed/ImagesListView/ImagesListViewController.swift
+++ ImageFeed/ImagesListView/ImagesListViewController.swift
@@ -1,12 +1,9 @@
 import UIKit
 
-class ViewController: UIViewController {
+final class ImagesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
