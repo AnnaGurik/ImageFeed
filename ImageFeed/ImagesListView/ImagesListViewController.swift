f76ec1b9abff6d497f5fa660164c659ae86f30d1
AnnaGurik <minigurk@mail.ru>
2023-12-24 19:47:14 +0500

    add table view to images list view controller

--- ImageFeed/ImagesListView/ImagesListViewController.swift
+++ ImageFeed/ImagesListView/ImagesListViewController.swift
@@ -2,8 +2,23 @@ import UIKit
 
 final class ImagesListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 }
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate {}
