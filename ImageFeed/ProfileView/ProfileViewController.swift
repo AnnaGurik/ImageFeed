77d81770b5c87468345f3f1ffea402747388ef49
AnnaGurik <minigurk@mail.ru>
2023-12-31 02:56:50 +0500

    add outlets to profile view controller

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -1,1 +1,14 @@
-import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private weak var profileImage: UIImageView!
+    @IBOutlet private weak var logoutButton: UIButton!
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var loginNameLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
