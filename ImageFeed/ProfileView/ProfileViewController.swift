42279c4eec60da5b9861bd2f902cf53eff587e6f
AnnaGurik <minigurk@mail.ru>
2023-12-31 17:17:30 +0500

    code layout for profile view

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -1,14 +1,90 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+    private let profileImage: UIImageView = {
+        let image = UIImage(named: "profileImage")
+        
+        return UIImageView(image: image)
+    }()
     
-    @IBOutlet private weak var profileImage: UIImageView!
-    @IBOutlet private weak var logoutButton: UIButton!
-    @IBOutlet private weak var nameLabel: UILabel!
-    @IBOutlet private weak var loginNameLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
+    private let logoutButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "logoutButton"), for: .normal)
+        
+        return button
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 23, weight: .semibold)
+        label.textColor = .white
+        label.text = "Екатерина Новикова"
+        
+        return label
+    }()
+    
+    private let loginNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0)
+        label.text = "@ekaterina_nov"
+        
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .white
+        label.text = "Hello, World!"
+        
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1.0)
+        addSubviews()
+        setupLayout()
+    }
+    
+    private func addSubviews() {
+        let subviews: [UIView] = [profileImage, logoutButton, nameLabel, loginNameLabel, descriptionLabel]
+        subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        subviews.forEach { view.addSubview($0) }
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            profileImage.widthAnchor.constraint(equalToConstant: 70),
+            profileImage.heightAnchor.constraint(equalToConstant: 70),
+            profileImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profileImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            logoutButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 55),
+            logoutButton.leadingAnchor.constraint(greaterThanOrEqualTo: profileImage.trailingAnchor, constant: 0),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            loginNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+        
     }
 }
