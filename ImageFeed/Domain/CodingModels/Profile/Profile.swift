3f264ad57b352baab7e0ef41bbccbea37b806921
AnnaGurik <minigurk@mail.ru>
2024-01-13 18:02:19 +0300

    create Profile model file

--- ImageFeed/Domain/CodingModels/Profile/Profile.swift
+++ ImageFeed/Domain/CodingModels/Profile/Profile.swift
@@ -1,19 +1,5 @@
 import Foundation
 
-struct ProfileResult: Codable {
-    let username: String
-    let firstName: String
-    let lastName: String
-    let bio: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case username
-        case firstName = "first_name"
-        case lastName = "last_name"
-        case bio
-    }
-}
-
 struct Profile {
     let username: String
     let name: String
